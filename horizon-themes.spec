8d24901397160480ee8d40866ae334e556b4a93a
Microwave <121560464+Micro856@users.noreply.github.com>
2026-01-29 21:09:38 +0100

    Add files via upload

---
+++ horizon-themes.spec
@@ -0,0 +1,40 @@
+Name:       horizon-themes
+Version:    1
+Release:    1%{?dist}
+Summary:    Horizon themes
+License:    GPLv3+
+
+URL:        https://github.com/horizonlinux/horizon-themes
+Source0:    https://github.com/horizonlinux/horizon-themes
+
+%description
+Horizon themes
+
+%prep
+%setup -q
+
+%build
+
+%install
+mkdir -p $RPM_BUILD_ROOT%{_datadir}/plasma/look-and-feel/io.github.horizonlinux.HorizonDark/contents/layouts
+mkdir -p $RPM_BUILD_ROOT%{_datadir}/plasma/look-and-feel/io.github.horizonlinux.HorizonLight/contents/layouts
+
+install -p -m 644 io.github.horizonlinux.HorizonLight/contents/layouts/org.kde.plasma.desktop-layout.js $RPM_BUILD_ROOT%{_datadir}/plasma/look-and-feel/io.github.horizonlinux.HorizonLight/contents/layouts/org.kde.plasma.desktop-layout.js
+install -p -m 644 io.github.horizonlinux.HorizonLight/contents/defaults $RPM_BUILD_ROOT%{_datadir}/plasma/look-and-feel/io.github.horizonlinux.HorizonLight/contents/defaults
+install -p -m 644 io.github.horizonlinux.HorizonLight/metadata.json $RPM_BUILD_ROOT%{_datadir}/plasma/look-and-feel/io.github.horizonlinux.HorizonLight/metadata.json
+
+install -p -m 644 io.github.horizonlinux.HorizonDark/contents/layouts/org.kde.plasma.desktop-layout.js $RPM_BUILD_ROOT%{_datadir}/plasma/look-and-feel/io.github.horizonlinux.HorizonDark/contents/layouts/org.kde.plasma.desktop-layout.js
+install -p -m 644 io.github.horizonlinux.HorizonDark/contents/defaults $RPM_BUILD_ROOT%{_datadir}/plasma/look-and-feel/io.github.horizonlinux.HorizonDark/contents/defaults
+install -p -m 644 io.github.horizonlinux.HorizonDark/metadata.json $RPM_BUILD_ROOT%{_datadir}/plasma/look-and-feel/io.github.horizonlinux.HorizonDark/metadata.json
+
+%files
+%{_datadir}/plasma/look-and-feel/io.github.horizonlinux.HorizonDark/*
+%{_datadir}/plasma/look-and-feel/io.github.horizonlinux.HorizonDark/contents/*
+%{_datadir}/plasma/look-and-feel/io.github.horizonlinux.HorizonDark/contents/layouts/*
+%{_datadir}/plasma/look-and-feel/io.github.horizonlinux.HorizonLight/*
+%{_datadir}/plasma/look-and-feel/io.github.horizonlinux.HorizonLight/contents/*
+%{_datadir}/plasma/look-and-feel/io.github.horizonlinux.HorizonLight/contents/layouts/*
+
+%changelog
+* Thu Jan 29 2026 Marcel Mrówka <micro.mail88@gmail.com>
+- package created
---
+++ io.github.horizonlinux.HorizonLight/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,99 @@
+var plasma = getApiVersion(1);
+
+var layout = {
+    "desktops": [
+        {
+            "applets": [
+            ],
+            "config": {
+                "/": {
+                    "ItemGeometries-1512x949": "",
+                    "ItemGeometriesHorizontal": "",
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/ConfigDialog": {
+                    "DialogHeight": "630",
+                    "DialogWidth": "810"
+                },
+                "/Wallpaper/org.kde.image/General": {
+                    "Image": "file:///usr/share/wallpapers/ZlataPraha/",
+                    "SlidePaths": "/usr/share/wallpapers/"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        }
+    ],
+    "panels": [
+        {
+            "alignment": "center",
+            "applets": [
+                {
+                    "config": {
+                        "/": {
+                            "PreloadWeight": "100",
+                            "popupHeight": "511",
+                            "popupWidth": "647"
+                        },
+                        "/General": {
+                            "favoritesPortedToKAstats": "true"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.kickoff"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.pager"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.icontasks"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.marginsseparator"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.systemtray"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "popupHeight": "400",
+                            "popupWidth": "560"
+                        },
+                        "/Appearance": {
+                            "fontWeight": "400"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.digitalclock"
+                }
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "2",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                }
+            },
+            "height": 2,
+            "hiding": "normal",
+            "location": "top",
+            "maximumLength": 84,
+            "minimumLength": 84,
+            "offset": 0
+        }
+    ],
+    "serializationFormatVersion": "1"
+}
+;
+
+plasma.loadSerializedLayout(layout);
